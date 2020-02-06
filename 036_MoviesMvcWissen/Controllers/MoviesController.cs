d254f525b6049e5fc8f490206013527f74f99099
Cagil Alsac <cagil.alsac@wissenakademie.com>
2020-02-06 21:33:09 +0300

    MoviesV05

--- 036_MoviesMvcWissen/Controllers/MoviesController.cs
+++ 036_MoviesMvcWissen/Controllers/MoviesController.cs
@@ -2,9 +2,10 @@
 using _036_MoviesMvcWissen.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Globalization;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
 
 namespace _036_MoviesMvcWissen.Controllers
@@ -16,26 +17,106 @@ namespace _036_MoviesMvcWissen.Controllers
         // GET: Movies
         public ViewResult Index()
         {
-            var model = db.Movies.ToList();
+            //var model = db.Movies.ToList();
+            var model = GetList();
+            //ViewBag.count = model.Count;
+            ViewData["count"] = model.Count;
             return View(model);
+        }
+
+        [NonAction]
+        public List<Movie> GetList(bool removeSession = true)
+        {
+            List<Movie> entities;
+            if (removeSession)
+                Session.Remove("movies");
+            if (Session["movies"] == null || removeSession)
+            {
+                entities = db.Movies.ToList();
+                Session["movies"] = entities;
+            }
+            else
+            {
+                entities = Session["movies"] as List<Movie>;
+            }
+            return entities;
+        }
+
+        public ActionResult GetMoviesFromSession()
+        {
+            var model = GetList(false);
+            ViewBag.count = model.Count;
+            return View("Index", model);
         }
 
         [HttpGet]
         public ActionResult Add()
         {
-            return View();
+            ViewBag.Message = "Please enter movie information...";
+            //return View();
+            return new ViewResult();
         }
 
         [HttpPost]
-        public RedirectToRouteResult Add(string Name, string ProductionYear, string BoxOfficeReturn)
+        public RedirectToRouteResult Add(string Name, int ProductionYear, string BoxOfficeReturn)
         {
             var entity = new Movie()
             {
                 Name = Name,
-                ProductionYear = ProductionYear,
+                ProductionYear = ProductionYear.ToString(),
                 BoxOfficeReturn = Convert.ToDouble(BoxOfficeReturn.Replace(",", "."), CultureInfo.InvariantCulture)
             };
             db.Movies.Add(entity);
+            db.SaveChanges();
+            TempData["Info"] = "Record successfully saved to database.";
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Id is required!");
+            var model = db.Movies.Find(id.Value);
+            List<SelectListItem> years = new List<SelectListItem>();
+            SelectListItem year;
+            for (int i = DateTime.Now.Year; i >= 2000; i--)
+            {
+                year = new SelectListItem() { Value = i.ToString(), Text = i.ToString() };
+                years.Add(year);
+            }
+            ViewBag.Years = new SelectList(years, "Value", "Text", model.ProductionYear);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "Id, Name, ProductionYear")]Movie movie, string BoxOfficeReturn)
+        {
+            var entity = db.Movies.SingleOrDefault(e => e.Id == movie.Id);
+            entity.Name = movie.Name;
+            entity.ProductionYear = movie.ProductionYear;
+            entity.BoxOfficeReturn = Convert.ToDouble(BoxOfficeReturn.Replace(",", "."), CultureInfo.InvariantCulture);
+            db.Entry(entity).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToRoute(new { controller = "Movies", action = "Index" });
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Id is required!");
+            var model = db.Movies.FirstOrDefault(e => e.Id == id.Value);
+            return View(model);
+        }
+
+        [ActionName("Delete")]
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (!id.HasValue)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Id is required!");
+            var entity = db.Movies.Find(id);
+            db.Movies.Remove(entity);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
